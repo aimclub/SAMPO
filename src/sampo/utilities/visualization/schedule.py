3e0262237d26c94c67f6c71cac5ef1aa387c2c15
nast1415 <nast1415@gmail.com>
2022-12-01 13:28:51 +0300

    gant chart visualization fixed

--- src/sampo/utilities/visualization/schedule.py
+++ src/sampo/utilities/visualization/schedule.py
@@ -16,6 +16,11 @@ def schedule_gant_chart_fig(schedule_dataframe: pd.DataFrame,
     :param visualization:
     :param schedule_dataframe: Pandas DataFrame with the information about schedule
     """
+    schedule_start = schedule_dataframe.loc[:, 'start'].min()
+    schedule_finish = schedule_dataframe.loc[:, 'finish'].max()
+    visualization_start_delta = timedelta(days=2)
+    visualization_finish_delta = timedelta(days=(schedule_finish - schedule_start).days // 3)
+
     fig = px.timeline(schedule_dataframe, x_start='start', x_end='finish', y='idx', hover_name='task_name',
                       color=schedule_dataframe.loc[:, 'contractor'],
                       hover_data=['workers', 'contractor'],
@@ -29,8 +34,8 @@ def schedule_gant_chart_fig(schedule_dataframe: pd.DataFrame,
                      type="category")
 
     fig.update_xaxes(type="date",
-                     range=[schedule_dataframe.loc[:, 'start'].min() - timedelta(days=2),
-                            schedule_dataframe.loc[:, 'finish'].max() + timedelta(days=40)],
+                     range=[schedule_start - visualization_start_delta,
+                            schedule_finish + visualization_finish_delta],
                      title_text='Date')
 
     fig.update_layout(autosize=True, font_size=12)

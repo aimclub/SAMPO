87acee90b253f46e6740b132817b98f29d7c5928
nast1415 <nast1415@gmail.com>
2022-12-01 14:38:33 +0300

    gant chart visualization fixed

--- src/sampo/utilities/schedule.py
+++ src/sampo/utilities/schedule.py
@@ -33,13 +33,21 @@ def merge_split_stages(task_df: pd.DataFrame) -> pd.Series:
     :return: pd.Series with the full information about the task
     """
     if len(task_df) == 1:
+        print(task_df.loc[0, 'workers'])
         return task_df.loc[0, :]
     else:
         df = task_df.iloc[-1:].reset_index(drop=True)
         for column in ['task_id', 'task_name']:
             df.loc[0, column] = df.loc[0, column].split('_')[0]  # fix task id and name
+
+        df.loc[0, 'volume'] = sum(task_df.loc[:, 'volume'])
+        df.loc[0, 'workers'] = task_df.loc[0, 'workers']
+
         # fix task's start time and duration
         df.loc[0, 'start'] = task_df.loc[0, 'start']
         df.loc[0, 'finish'] = task_df.loc[len(task_df) - 1, 'finish']
         df.loc[0, 'duration'] = (df.loc[0, 'finish'] - df.loc[0, 'start']).days + 1
+
+        # print("Названия колонок df:", df.columns)
+        print(df.loc[0, 'workers'])
         return df.loc[0, :]

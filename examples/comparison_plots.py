2b2418fccbaa3dd24163b1a0faa9748e3b367295
Quarter <stas.batalenkov@mail.ru>
2022-12-22 12:20:14 +0300

    Generalized all plots // TODO Add support for genetics

--- examples/comparison_plots.py
+++ examples/comparison_plots.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Dict
 
 import matplotlib.pyplot as plt
 
@@ -14,18 +15,18 @@ global_algo_block_type_frequencies = defaultdict(lambda: defaultdict(int))
 global_algo_bg_type_frequencies = defaultdict(lambda: defaultdict(int))
 
 # algo -> downtime
-global_algo_downtime = defaultdict(int)
+global_algo_downtimes = defaultdict(int)
 # algo, block type -> downtime
-global_algo_block_type_downtime = defaultdict(lambda: defaultdict(int))
+global_algo_block_type_downtimes = defaultdict(lambda: defaultdict(int))
 # algo, block graph type -> downtime
-global_algo_bg_type_downtime = defaultdict(lambda: defaultdict(int))
+global_algo_bg_type_downtimes = defaultdict(lambda: defaultdict(int))
 
 run_mode_index = 0
 
 algo_labels = ['HEFTAddEnd', 'HEFTAddBetween', 'Topological',
                'Genetic[\ngenerations=50,size_selection=None,\nmutate_order=None,mutate_resources=None]']
 
-
+# parsing raw data
 for run_index in range(5):
     with open(f'algorithms_comparison_block_size_{run_mode_index}_{run_index}.txt', 'r') as f:
         mode = None
@@ -65,8 +66,8 @@ for run_index in range(5):
                 finished = True
                 downtimes = [int(downtime) for downtime in line.split(' ')]
                 for algo_ind, algo in enumerate(algos):
-                    global_algo_downtime[algo] += downtimes[algo_ind]
-                    global_algo_bg_type_downtime[bg_info[0]][algo] += downtimes[algo_ind]
+                    global_algo_downtimes[algo] += downtimes[algo_ind]
+                    global_algo_bg_type_downtimes[bg_info[0]][algo] += downtimes[algo_ind]
                 i += 1
                 continue
 
@@ -79,22 +80,22 @@ for run_index in range(5):
                 global_algo_block_type_frequencies[algo][bg_info[i + 1]] += 1
             i += 1
 
-# plt.subplots_adjust(top=100, wspace=.5, hspace=.5)
 
 # general comparison
-# algo_labels = list(global_algo_frequencies.keys())
-algo_freq = list(global_algo_frequencies.values())
-
-centers = range(len(algo_labels))
-_, ax = plt.subplots(figsize=(10, 7))
-ax.bar(centers, algo_freq, tick_label=algo_labels)
-plt.suptitle('Algorithms block receive count', fontsize=24)
-
-plt.show()
+def compare_algos_general(title: str, compare_dict: Dict):
+    centers = range(len(algo_labels))
+    _, ax = plt.subplots(figsize=(10, 7))
+    ax.bar(centers, compare_dict.values(), tick_label=algo_labels)
+    plt.suptitle(title, fontsize=24)
+    plt.show()
 
 
-def compare_algos(title, compare_dict):
-    # block graph type comparison
+compare_algos_general('Algorithms block receive count - average', global_algo_frequencies)
+compare_algos_general('Algorithms downtimes - average', global_algo_downtimes)
+
+
+# block graph type comparison
+def compare_algos_bg_type(title: str, compare_dict: Dict):
     fig = plt.figure(figsize=(14, 10))
     fig.suptitle(title, fontsize=32)
 
@@ -124,34 +125,38 @@ def compare_algos(title, compare_dict):
     plt.show()
 
 
-compare_algos('Received blocks - algorithms with graph types comparison', global_algo_bg_type_frequencies)
-compare_algos('Downtimes - algorithms with graph types comparison', global_algo_bg_type_downtime)
+compare_algos_bg_type('Received blocks - algorithms with graph types comparison', global_algo_bg_type_frequencies)
+compare_algos_bg_type('Downtimes - algorithms with graph types comparison', global_algo_bg_type_downtimes)
+
 
 # block type comparison
-fig = plt.figure(figsize=(14, 10))
-fig.suptitle('Algorithms with block types comparison', fontsize=32)
-freq = list(global_algo_block_type_frequencies.items())[0]
-ax1 = fig.add_subplot(221)
-ax1.set(title=algo_labels[0], xticks=[], yticks=[])
-ax1.bar(range(len(freq[1])), freq[1].values(), tick_label=list(freq[1].keys()))
-freq = list(global_algo_block_type_frequencies.items())[1]
-ax2 = fig.add_subplot(222)
-ax2.set(title=algo_labels[1], xticks=[], yticks=[])
-ax2.bar(range(len(freq[1])), freq[1].values(), tick_label=list(freq[1].keys()))
-freq = list(global_algo_block_type_frequencies.items())[2]
-ax3 = fig.add_subplot(223)
-ax3.set(title=algo_labels[2], xticks=[], yticks=[])
-ax3.bar(range(len(freq[1])), freq[1].values(), tick_label=list(freq[1].keys()))
-freq = list(global_algo_block_type_frequencies.items())[3]
-ax4 = fig.add_subplot(224)
-ax4.set(title=algo_labels[3], xticks=[], yticks=[])
-ax4.bar(range(len(freq[1])), freq[1].values(), tick_label=list(freq[1].keys()))
+def compare_algos_block_type(title: str, compare_dict: Dict):
+    fig = plt.figure(figsize=(14, 10))
+    fig.suptitle(title, fontsize=32)
+    freq = list(compare_dict.items())[0]
+    ax1 = fig.add_subplot(221)
+    ax1.set(title=algo_labels[0], xticks=[], yticks=[])
+    ax1.bar(range(len(freq[1])), freq[1].values(), tick_label=list(freq[1].keys()))
+    freq = list(compare_dict.items())[1]
+    ax2 = fig.add_subplot(222)
+    ax2.set(title=algo_labels[1], xticks=[], yticks=[])
+    ax2.bar(range(len(freq[1])), freq[1].values(), tick_label=list(freq[1].keys()))
+    freq = list(compare_dict.items())[2]
+    ax3 = fig.add_subplot(223)
+    ax3.set(title=algo_labels[2], xticks=[], yticks=[])
+    ax3.bar(range(len(freq[1])), freq[1].values(), tick_label=list(freq[1].keys()))
+    freq = list(compare_dict.items())[3]
+    ax4 = fig.add_subplot(224)
+    ax4.set(title=algo_labels[3], xticks=[], yticks=[])
+    ax4.bar(range(len(freq[1])), freq[1].values(), tick_label=list(freq[1].keys()))
 
-plt.subplots_adjust(left=0.1,
-                    bottom=0.1,
-                    right=0.9,
-                    top=0.9,
-                    wspace=0.4,
-                    hspace=0.4)
-plt.show()
+    plt.subplots_adjust(left=0.1,
+                        bottom=0.1,
+                        right=0.9,
+                        top=0.9,
+                        wspace=0.4,
+                        hspace=0.4)
+    plt.show()
 
+
+compare_algos_block_type('Received blocks - algorithms with block types comparison', global_algo_block_type_frequencies)

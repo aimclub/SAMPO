b1e7d9ed2ca18c20c3d2ff14db2000b64d46ff84
Quarter <stas.batalenkov@mail.ru>
2022-12-27 17:59:55 +0300

    Added example for loading bg

---
+++ examples/obstruction_application.py
@@ -0,0 +1,55 @@
+import json
+from random import Random
+from typing import Dict
+
+from sampo.scheduler.base import Scheduler
+from sampo.scheduler.multi_agency.block_graph import BlockGraph
+from sampo.scheduler.multi_agency.multi_agency import Agent, Manager, ScheduledBlock
+from sampo.scheduler.utils.obstruction import OneInsertObstruction
+from sampo.schemas.contractor import Contractor
+from sampo.schemas.graph import WorkGraph
+
+
+def load_queues_bg(queues: list[list[(str, str)]], obstruction_prob: float, rand: Random = Random()):
+    wgs: list[WorkGraph] = [json.load(file) for queue in queues for file, _ in queue]
+    obstructions: list[WorkGraph] = [json.load(obstruction_file) for queue in queues for _, obstruction_file in queue]
+
+    def obstruction_getter(i: int):
+        return OneInsertObstruction.from_static_graph(obstruction_prob, rand, obstructions[i])
+
+    bg = BlockGraph.pure(wgs, obstruction_getter)
+
+    index = 0  # global wg index in `wgs`
+    nodes_prev = []
+    for queue in queues:
+        nodes = [bg[wgs[i].start.id] for i in range(index, index + len(queue))]
+
+        # generate edges
+        generated_edges = 0
+        for i, node in enumerate(nodes[:-2]):
+            if i >= len(nodes_prev):
+                break
+            BlockGraph.add_edge(node, nodes_prev[i])
+            generated_edges += 1
+
+        for i, node in enumerate(nodes):
+            if i >= len(nodes_prev):
+                break
+            # we are going in reverse to fill edges that are not covered by previous cycle
+            BlockGraph.add_edge(node, nodes_prev[-i])
+            generated_edges += 1
+
+        nodes_prev = nodes
+
+    return bg
+
+
+def run_example(obstruction_prob: float, rand: Random, queues_with_obstructions: list[list[(str, str)]],
+                schedulers: list[Scheduler], contractors: list[Contractor]) -> Dict[str, ScheduledBlock]:
+    agents = [Agent(f'Agent {i}', schedulers[i % len(schedulers)], [contractor])
+              for i, contractor in enumerate(contractors)]
+    manager = Manager(agents)
+
+    bg = load_queues_bg(queues_with_obstructions, obstruction_prob, rand)
+
+    return manager.manage_blocks(bg, logger=print)

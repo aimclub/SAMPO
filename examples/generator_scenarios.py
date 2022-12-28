6314824a15315c020afbc7fb0fdd05a71b250351
angrymuskrat <mlhakov2011@gmail.com>
2022-12-28 14:15:54 +0300

    union graph_insertion and generator_scenarios examples

--- examples/generator_scenarios.py
+++ examples/generator_scenarios.py
@@ -4,6 +4,9 @@ from itertools import chain
 from sampo.generator import SimpleSynthetic, get_contractor_by_wg
 from sampo.generator.pipeline.extension import extend_names, extend_resources
 from sampo.scheduler.heft.base import HEFTScheduler
+from sampo.schemas.graph import WorkGraph
+from sampo.structurator import graph_in_graph_insertion, work_graph_ids_simplification, graph_restructuring
+from sampo.utilities.visualization.work_graph import work_graph_fig
 
 rand = random.Random(10)
 p_rand = SimpleSynthetic(rand=231)
@@ -39,3 +42,23 @@ print("\nCheck gen contractor by wg extension")
 
 res_names_new_c = len(set(get_contractor_by_wg(new_wg, scaler=1).workers.keys()))
 print(f"works in new by wg: {res_names_new_wg}, by contractor: {res_names_new_c}")
+
+
+def plot_wg(wg: WorkGraph) -> None:
+    _ = work_graph_fig(wg, (14, 8), legend_shift=4, show_names=True, text_size=4)
+    plt.show()
+
+
+srand = SimpleSynthetic(34)
+wg_master = srand.work_graph(cluster_counts=1)
+wg_slave = srand.work_graph(cluster_counts=1)
+
+union_wg = graph_in_graph_insertion(wg_master, wg_master.start, wg_master.finish, wg_slave)
+union_wg_simply = work_graph_ids_simplification(union_wg, id_offset=1000)
+union_wg_restructured = graph_restructuring(union_wg_simply)
+
+plot_wg(wg_master)
+plot_wg(wg_slave)
+plot_wg(union_wg)
+plot_wg(union_wg_simply)
+plot_wg(union_wg_restructured)

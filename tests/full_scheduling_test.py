14a80d51fc029a3caccef5212f32de96d1932a91
Quarter <stas.batalenkov@mail.ru>
2023-01-14 20:39:47 +0300

    Added lag_optimization to full_scheduling_test

--- tests/full_scheduling_test.py
+++ tests/full_scheduling_test.py
@@ -3,11 +3,13 @@ from sampo.generator.pipeline.types import SyntheticGraphType
 from sampo.scheduler.base import SchedulerType
 from sampo.scheduler.generate import generate_schedule
 from sampo.schemas.time import Time
+from sampo.structurator import graph_restructuring
 
 
 def test_schedule_synthetic(setup_contractors):
     ss = SimpleSynthetic(rand=231)
     wg = ss.work_graph(SyntheticGraphType.General, 100, 200)
+    wg = graph_restructuring(wg, use_lag_edge_optimization=True)
 
     for scheduler_type in list(SchedulerType):
         schedule = generate_schedule(scheduler_type, None, wg, setup_contractors, validate_schedule=True)

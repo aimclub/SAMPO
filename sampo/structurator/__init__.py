e73fc84cd60c4cce3487dec286eb08280e51584a
resurrectiontent <asvoskresenskii@gmail.com>
2023-02-15 19:46:35 +0300

    fix merging stages + introduce STAGE_SEP constant

--- sampo/structurator/__init__.py
+++ sampo/structurator/__init__.py
@@ -1,3 +1,3 @@
-from sampo.structurator.base import graph_restructuring
+from sampo.structurator.base import graph_restructuring, STAGE_SEP
 from sampo.structurator.graph_insertion import graph_in_graph_insertion
 from sampo.structurator.light_modifications import work_graph_ids_simplification

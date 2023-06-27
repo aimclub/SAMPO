73c9605c81b82ff35826e0ee776c578c0d5836c6
Lumograul <lumograul@gmail.com>
2023-06-27 04:17:36 +0300

    Added ability to accept list[GraphNode] in insert_work_unit function

--- sampo/structurator/insert_wu.py
+++ sampo/structurator/insert_wu.py
@@ -3,10 +3,9 @@ from sampo.schemas.works import WorkUnit
 from sampo.structurator.prepare_wg_copy import prepare_work_graph_copy, new_start_finish
 
 
-# TODO make function accept list[GraphNode] for parents and children
 def insert_work_unit(original_wg: WorkGraph, inserted_wu: WorkUnit,
-                     parents_edges: list[tuple[GraphNode, float, EdgeType]],
-                     children_edges: list[tuple[GraphNode, float, EdgeType]],
+                     parents_edges: list[GraphNode] or list[tuple[GraphNode, float, EdgeType]],
+                     children_edges: list[GraphNode] or list[tuple[GraphNode, float, EdgeType]],
                      change_id: bool = True) -> WorkGraph:
     """
     Inserts new node, based on given Work Unit, in the Work Graph
@@ -19,16 +18,30 @@ def insert_work_unit(original_wg: WorkGraph, inserted_wu: WorkUnit,
     """
     copied_nodes, original_old_to_new_ids = prepare_work_graph_copy(original_wg, change_id=change_id)
 
-    new_parents_edges = [(copied_nodes[original_old_to_new_ids[parent.id]], lag, edge_type)
-                         for parent, lag, edge_type in parents_edges]
-    new_children_edges = [(copied_nodes[original_old_to_new_ids[child.id]], lag, edge_type)
-                          for child, lag, edge_type in children_edges]
+    new_parents_edges = _new_edges(copied_nodes, original_old_to_new_ids, parents_edges)
+    new_children_edges = _new_edges(copied_nodes, original_old_to_new_ids, children_edges)
 
     new_node = GraphNode(inserted_wu, [])
     new_node.add_parents(new_parents_edges)
-    for child, lag, edge in new_children_edges:
-        child.add_parents([(new_node, lag, edge)])
+    if isinstance(new_children_edges[0], GraphNode):
+        for child in new_children_edges:
+            child.add_parents([new_node])
+    elif isinstance(children_edges[0], tuple):
+        for child, lag, edge in new_children_edges:
+            child.add_parents([(new_node, lag, edge)])
 
     new_start, new_finish = new_start_finish(original_wg, copied_nodes, original_old_to_new_ids)
 
     return WorkGraph(new_start, new_finish)
+
+
+def _new_edges(copied_nodes: dict[str, GraphNode], original_old_to_new_ids: dict[str, str],
+               edges: list[GraphNode] or list[tuple[GraphNode, float, EdgeType]]) \
+        -> list[GraphNode] or list[tuple[GraphNode, float, EdgeType]]:
+    if isinstance(edges[0], GraphNode):
+        return [copied_nodes[original_old_to_new_ids[parent.id]] for parent in edges]
+    elif isinstance(edges[0], tuple):
+        return [(copied_nodes[original_old_to_new_ids[parent.id]], lag, edge_type)
+                for parent, lag, edge_type in edges]
+    else:
+        return []

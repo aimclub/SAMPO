20a9f7e321e4f30a39f628558d0c9eaa4a1436d6
Quarter <stas.batalenkov@mail.ru>
2023-07-07 12:13:55 +0300

    Fixed issues

--- tests/scheduler/genetic/full_scheduling.py
+++ tests/scheduler/genetic/full_scheduling.py
@@ -2,7 +2,7 @@ from sampo.scheduler.genetic.base import GeneticScheduler
 
 
 def test_multiprocessing(setup_scheduler_parameters):
-    setup_wg, setup_contractors = setup_scheduler_parameters
+    setup_wg, setup_contractors, setup_landscape = setup_scheduler_parameters
 
     genetic = GeneticScheduler(number_of_generation=500,
                                mutate_order=1.0,

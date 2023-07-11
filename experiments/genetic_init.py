f0c39b8ddff154db3ca5cd0818f7268bfafcb1cc
Quarter <stas.batalenkov@mail.ru>
2023-07-11 15:51:18 +0300

    Made experiments

--- experiments/genetic_init.py
+++ experiments/genetic_init.py
@@ -1,59 +1,58 @@
-from uuid import uuid4
+from pathos.multiprocessing import ProcessingPool
 
-from sampo.generator import SimpleSynthetic
+from sampo.generator.base import SimpleSynthetic
+from sampo.generator.environment.contractor_by_wg import get_contractor_by_wg
 from sampo.scheduler.genetic.base import GeneticScheduler
-from sampo.schemas.contractor import Contractor
-from sampo.schemas.graph import WorkGraph
-from sampo.schemas.resources import Worker
+from sampo.schemas.time import Time
 
 
-def generate_contractors(wg: WorkGraph, num_contractors: int, contractor_min_resources: int) -> list[Contractor]:
-    resource_req: dict[str, int] = {}
-    resource_req_count: dict[str, int] = {}
+def run_test(args) -> list[tuple[Time, Time]]:
+    graph_size, iterations = args
 
-    for node in wg.nodes:
-        for req in node.work_unit.worker_reqs:
-            resource_req[req.kind] = max(contractor_min_resources,
-                                         resource_req.get(req.kind, 0) + (req.min_count + req.max_count) // 2)
-            resource_req_count[req.kind] = resource_req_count.get(req.kind, 0) + 1
+    result = []
+    for i in range(iterations):
+        ss = SimpleSynthetic()
+        wg = ss.work_graph(top_border=graph_size)
+        contractors = [get_contractor_by_wg(wg)]
 
-    for req in resource_req.keys():
-        resource_req[req] //= resource_req_count[req]
+        baseline_genetic = GeneticScheduler(mutate_order=1.0,
+                                            mutate_resources=1.0,
+                                            size_selection=200,
+                                            size_of_population=200)
 
-    # contractors are the same and universal(but multiple)
-    contractors = []
-    for i in range(num_contractors):
-        contractor_id = str(uuid4())
-        contractors.append(Contractor(id=contractor_id,
-                                      name="OOO Berezka",
-                                      workers={name: Worker(str(uuid4()), name, count, contractor_id=contractor_id)
-                                               for name, count in resource_req.items()},
-                                      equipments={}))
-    return contractors
+        optimized_genetic = GeneticScheduler(mutate_order=1.0,
+                                             mutate_resources=1.0,
+                                             size_selection=200,
+                                             size_of_population=200)
+        optimized_genetic.set_weights([14, 11, 1, 1, 1, 1, 10])
 
+        baseline_result = baseline_genetic.schedule(wg, contractors)
+        my_result = optimized_genetic.schedule(wg, contractors)
 
-GRAPH_SIZE = 1000
-BORDER_RADIUS = 20
+        result.append((baseline_result.execution_time, my_result.execution_time))
 
-ss = SimpleSynthetic()
-wg = ss.work_graph(top_border=GRAPH_SIZE)
-contractors = generate_contractors(wg, 1, 0)
+    return result
 
 
-baseline_genetic = GeneticScheduler(mutate_order=1.0,
-                                    mutate_resources=1.0,
-                                    size_selection=200,
-                                    size_of_population=200)
+if __name__ == '__main__':
+    num_iterations = 4
 
-optimized_genetic = GeneticScheduler(mutate_order=1.0,
-                                     mutate_resources=1.0,
-                                     size_selection=200,
-                                     size_of_population=200)
-optimized_genetic.set_weights([14, 11, 1, 1, 1, 1, 10])
+    sizes = [100 * i for i in range(1, num_iterations + 1)]
+    iterations = [5 - i for i in range(1, num_iterations + 1)]
 
-baseline_result = baseline_genetic.schedule(wg, contractors)
-my_result = optimized_genetic.schedule(wg, contractors)
+    with ProcessingPool(10) as p:
+        results_by_size = p.map(run_test, zip(sizes, iterations))
 
-print(f'Graph size: {wg.vertex_count}')
-print(f'Baseline result: {baseline_result.execution_time}')
-print(f'My result: {my_result.execution_time}')
+        print()
+        with open('genetic_init_res.txt', 'w') as f:
+            for graph_size, result_list in zip(sizes, results_by_size):
+                global_ratio = 0
+                for baseline_result, my_result in result_list:
+                    ratio = baseline_result / my_result
+                    global_ratio += ratio
+                global_ratio /= len(result_list)
+
+                res_string = f'Size: {graph_size}, upgrade ratio: {global_ratio}'
+                print(res_string)
+                f.write(res_string)
+                f.write('\n')

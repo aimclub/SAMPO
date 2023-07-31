047e357311ce588961dffcd9332418fa920f4102
Ivan <dsvanoha@gmail.com>
2023-07-31 13:04:19 +0300

    Now data can be represented as .csv files. Upgraded function of parsing input data: contractor can be built from .csv file not only work graph.

--- sampo/userinput/parser/csv_parser.py
+++ sampo/userinput/parser/csv_parser.py
@@ -4,8 +4,7 @@ from uuid import uuid4
 import pandas as pd
 from pandas import DataFrame
 
-from sampo.generator.environment.contractor_by_wg import get_contractor_by_wg
-from sampo.schemas.contractor import Contractor
+from sampo.schemas.contractor import Contractor, get_contractor_for_resources_schedule
 from sampo.schemas.graph import WorkGraph
 from sampo.schemas.resources import Worker
 from sampo.schemas.time_estimator import DefaultWorkEstimator, WorkTimeEstimator
@@ -22,14 +21,17 @@ class CSVParser:
                                    history_data: str | None = None,
                                    contractor_info: str | None = None,
                                    unique_work_names_mapper: NameMapper | None = None,
-                                   work_resource_estimator: WorkTimeEstimator = DefaultWorkEstimator()) \
+                                   work_resource_estimator: WorkTimeEstimator = DefaultWorkEstimator(),
+                                   contractors_number: int = 1) \
             -> (WorkGraph, Contractor):
         """
         Gets a WorkGraph and Contractors from file .csv
 
+        :param contractors_number: if we do not receive contractors, we need to know how many contractors the user wants,
+        for a generation
         :param history_data: path to history data .csv file
         :param contractor_info: path to contractor info .csv file
-        :param work_resource_estimator: work estimator that finds necessary resources, based on history data
+        :param work_resource_estimator: work estimator that finds necessary resources, based on the history data
         :param project_info: path to .csv file
         :return: WorkGraph
         """
@@ -37,7 +39,7 @@ class CSVParser:
         graph_df = pd.read_csv(project_info, sep=';', header=0) if isinstance(project_info,
                                                                               str) else project_info
         # 1. work with tasks' connections. Preprocessing and breaking loops
-        if history_data is None:
+        if history_data is None or (not(history_data is None) and 'predecessor_ids' in graph_df.columns):
             # 1.1. if we have info about predecessors in graph_df
             if 'predecessor_ids' not in graph_df.columns:
                 raise InputDataException(
@@ -63,6 +65,10 @@ class CSVParser:
         elif contractor_info is None:
             resources = [work_resource_estimator.find_work_resources(w[0], float(w[1]))
                          for w in works_info.loc[:, ['activity_name', 'volume']].to_numpy()]
+            contractors = [get_contractor_for_resources_schedule(resources,
+                                                                 contractor_id=str(i),
+                                                                 contractor_name='Contractor' + ' ' + str(i + 1))
+                           for i in range(contractors_number)]
         else:
             # if contractor info is given or contractor info and work resource estimator are received simultaneously
             contractor_df = pd.read_csv(contractor_info, sep=';', header=0) if isinstance(contractor_info,
@@ -79,6 +85,10 @@ class CSVParser:
                                equipments=dict())
                 )
             resource_names = contractor_df.columns[1:].to_list()
+            if len(contractors) == 0 and isinstance(work_resource_estimator, DefaultWorkEstimator):
+                raise InputDataException(
+                    'you have neither info about contractors nor work resource estimator.'
+                )
             resources = [work_resource_estimator.find_work_resources(w[0], float(w[1]), resource_names)
                          for w in works_info.loc[:, ['activity_name', 'volume']].to_numpy()]
 
@@ -89,8 +99,11 @@ class CSVParser:
         works_resources = topsort_graph_df(works_resources)
         work_graph = build_work_graph(works_resources, unique_res, resource_names)
 
-        # if we have no info about contractors or user receive empty .csv file
+        # if we have no info about contractors or the user send an empty .csv file
         if len(contractors) == 0:
-            contractors = get_contractor_by_wg(work_graph)
+            contractors = [get_contractor_for_resources_schedule(resources,
+                                                                 contractor_id=str(i),
+                                                                 contractor_name='Contractor' + ' ' + str(i + 1))
+                           for i in range(contractors_number)]
 
         return work_graph, contractors

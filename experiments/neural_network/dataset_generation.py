676a4b57f47c5bf3f16c7c2298092b75c5ae6343
Ivan <dsvanoha@gmail.com>
2023-09-04 12:47:17 +0300

    Cosmetic changes

--- experiments/neural_network/dataset_generation.py
+++ experiments/neural_network/dataset_generation.py
@@ -1,3 +1,6 @@
+import linecache
+import os
+import tracemalloc
 from multiprocessing import Pool
 
 import numpy as np
@@ -45,43 +48,43 @@ def argmin(array) -> int:
 #     return result
 
 
-# def display_top(snapshot, key_type='lineno', limit=3):
-#     snapshot = snapshot.filter_traces((
-#         tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-#         tracemalloc.Filter(False, "<unknown>"),
-#     ))
-#     top_stats = snapshot.statistics(key_type)
-#
-#     print("Top %s lines" % limit)
-#     for index, stat in enumerate(top_stats[:limit], 1):
-#         frame = stat.traceback[0]
-#         # replace "/path/to/module/file.py" with "module/file.py"
-#         filename = os.sep.join(frame.filename.split(os.sep)[-2:])
-#         print("#%s: %s:%s: %.1f KiB"
-#               % (index, filename, frame.lineno, stat.size / 1024))
-#         line = linecache.getline(frame.filename, frame.lineno).strip()
-#         if line:
-#             print('    %s' % line)
-#
-#     other = top_stats[limit:]
-#     if other:
-#         size = sum(stat.size for stat in other)
-#         print("%s other: %.1f KiB" % (len(other), size / 1024))
-#     total = sum(stat.size for stat in top_stats)
-#     print("Total allocated size: %.1f KiB" % (total / 1024))
+def display_top(snapshot, key_type='lineno', limit=3):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    # print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        # print("#%s: %s:%s: %.1f KiB"
+        #       % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        # if line:
+        #     print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        # print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
 
 
 def generate():
-    # tracemalloc.start()
+    tracemalloc.start()
     wg = ss.work_graph(top_border=GRAPHS_TOP_BORDER)
     encoding = encode_graph(wg)
     schedulers_results = [int(scheduler.schedule(wg, contractors).execution_time) for scheduler in schedulers]
     generated_label = argmin(schedulers_results)
-    # del wg
-    # del schedulers_results
+    del wg
+    del schedulers_results
 
-    # snapshot = tracemalloc.take_snapshot()
-    # display_top(snapshot)
+    snapshot = tracemalloc.take_snapshot()
+    display_top(snapshot)
     #
     # del wg
 

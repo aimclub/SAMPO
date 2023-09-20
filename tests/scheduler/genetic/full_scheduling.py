99739c21ba54cbcbef16fb5b605ca57f62fe7bf5
Egor <timegor248@gmail.com>
2023-09-20 16:05:52 +0300

    Replace number_of_generations in tests by 10

--- tests/scheduler/genetic/full_scheduling.py
+++ tests/scheduler/genetic/full_scheduling.py
@@ -4,7 +4,7 @@ from sampo.scheduler.genetic.base import GeneticScheduler
 def test_multiprocessing(setup_scheduler_parameters):
     setup_wg, setup_contractors, setup_landscape = setup_scheduler_parameters
 
-    genetic = GeneticScheduler(number_of_generation=100,
+    genetic = GeneticScheduler(number_of_generation=10,
                                mutate_order=0.05,
                                mutate_resources=0.005,
                                size_of_population=50)
--- tests/scheduler/resources_in_time/basic_res_test.py
+++ tests/scheduler/resources_in_time/basic_res_test.py
@@ -35,7 +35,7 @@ def test_genetic_deadline_planning(setup_scheduler_parameters):
     setup_wg, setup_contractors, landscape = setup_scheduler_parameters
 
     deadline = Time.inf() // 2
-    scheduler = GeneticScheduler(number_of_generation=50,
+    scheduler = GeneticScheduler(number_of_generation=10,
                                  mutate_order=0.05,
                                  mutate_resources=0.005,
                                  size_of_population=50,
@@ -88,7 +88,7 @@ def test_lexicographic_genetic_deadline_planning(setup_scheduler_parameters):
 
     print(f'Deadline time: {deadline}')
 
-    scheduler_combined = GeneticScheduler(number_of_generation=100,
+    scheduler_combined = GeneticScheduler(number_of_generation=10,
                                           mutate_order=0.05,
                                           mutate_resources=0.005,
                                           size_of_population=50,
@@ -98,7 +98,7 @@ def test_lexicographic_genetic_deadline_planning(setup_scheduler_parameters):
 
     scheduler_combined.set_deadline(deadline)
 
-    scheduler_lexicographic = GeneticScheduler(number_of_generation=100,
+    scheduler_lexicographic = GeneticScheduler(number_of_generation=10,
                                                mutate_order=0.05,
                                                mutate_resources=0.005,
                                                size_of_population=50,

86bfa7d2d7bb25bf02a6c4e72409635e3d40df5d
Quarter <stas.batalenkov@mail.ru>
2024-03-07 18:30:20 +0300

    -

--- sampo/native/modules/native/src/main.cpp
+++ sampo/native/modules/native/src/main.cpp
@@ -1,40 +1,26 @@
-//#include <iostream>
+#include <iostream>
+#include <string>
+
+#include <pybind11/embed.h>
+
+#include "native/python_deserializer.h"
+
+namespace py = pybind11;
+using namespace std;
+
+int main() {
+    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+
+    try {
+        py::module test_module = py::module_::import("extra.test");
+
+//        auto result = test_module.attr("get_args")().cast<py::tuple>();
 //
-//#include "Python.h"
-////#include <Python.h>
-//
-//using namespace std;
-//
-//int main() {
-//    PyObject *pName, *pModule, *pFunc;
-//    PyObject *pArgs, *pValue, *sys, *path;
-//
-//    Py_Initialize();
-//
-////    sys  = PyImport_ImportModule("sampo.schemas");
-////    path = PyObject_GetAttrString(sys, "WorkGraph");
-////    PyList_Append(path, PyUnicode_FromString("."));
-//
-//    pModule = PyImport_ImportModule("sampo.schemas.WorkGraph");
-//
-//    if (!pModule) {
-//        PyErr_Print();
-//        cout << "ERROR in pModule" << endl;
-//        exit(1);
-//    }
-//
-//    pFunc = PyObject_GetAttrString(pModule, "loadf");
-//    pArgs = PyTuple_New(1);
-//    PyTuple_SetItem(pArgs, 0, PyUnicode_FromString("wg.json"));
-//
-//    PyObject* wg = PyObject_CallObject(pFunc, pArgs);
-//
-//    if (!wg) {
-//        PyErr_Print();
-//        cout << "ERROR in WorkGraph#loadf" << endl;
-//        exit(1);
-//    }
-//
-//    Py_Finalize();
-//    return 0;
-//}
+//        cout << result[0].attr("vertex_count").cast<int>() << endl;
+        // PythonDeserializer::workGraph(result[0].ptr());
+
+    } catch (exception &e) {
+        cout << e.what();
+    }
+    return 0;
+}

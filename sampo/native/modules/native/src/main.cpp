f84d0a14d769c5d9bf0495eac52cbb2f5b4fb833
Quarter <stas.batalenkov@mail.ru>
2024-03-08 15:51:50 +0300

    -

--- sampo/native/modules/native/src/main.cpp
+++ sampo/native/modules/native/src/main.cpp
@@ -1,40 +1,46 @@
-//#include <iostream>
-//
-//#include "Python.h"
-////#include <Python.h>
-//
-//using namespace std;
-//
-//int main() {
-//    PyObject *pName, *pModule, *pFunc;
-//    PyObject *pArgs, *pValue, *sys, *path;
-//
-//    Py_Initialize();
-//
-////    sys  = PyImport_ImportModule("sampo.schemas");
-////    path = PyObject_GetAttrString(sys, "WorkGraph");
-////    PyList_Append(path, PyUnicode_FromString("."));
-//
-//    pModule = PyImport_ImportModule("sampo.schemas.WorkGraph");
-//
-//    if (!pModule) {
-//        PyErr_Print();
-//        cout << "ERROR in pModule" << endl;
-//        exit(1);
-//    }
-//
-//    pFunc = PyObject_GetAttrString(pModule, "loadf");
-//    pArgs = PyTuple_New(1);
-//    PyTuple_SetItem(pArgs, 0, PyUnicode_FromString("wg.json"));
-//
-//    PyObject* wg = PyObject_CallObject(pFunc, pArgs);
-//
-//    if (!wg) {
-//        PyErr_Print();
-//        cout << "ERROR in WorkGraph#loadf" << endl;
-//        exit(1);
-//    }
-//
-//    Py_Finalize();
-//    return 0;
-//}
+#include <iostream>
+
+#include "Python.h"
+//#include <Python.h>
+
+using namespace std;
+
+int main() {
+    PyObject *pName, *pModule, *pFunc;
+    PyObject *pArgs, *pValue, *sys, *path;
+
+    Py_Initialize();
+
+//    sys  = PyImport_ImportModule("sys");
+//    path = PyObject_GetAttrString(sys, "path");
+//    PyList_Append(path, PyUnicode_FromString("."));
+
+    pModule = PyImport_ImportModule("extra.test");
+
+    if (!pModule) {
+        PyErr_Print();
+        cout << "ERROR in pModule" << endl;
+        exit(1);
+    }
+
+    pFunc = PyObject_GetAttrString(pModule, "get_args");
+
+    if (!pFunc) {
+        PyErr_Print();
+        cout << "ERROR in pFunc" << endl;
+        exit(1);
+    }
+
+    pArgs = PyTuple_New(0);
+
+    PyObject* wg = PyObject_CallObject(pFunc, pArgs);
+
+    if (!wg) {
+        PyErr_Print();
+        cout << "ERROR in WorkGraph#loadf" << endl;
+        exit(1);
+    }
+
+    Py_Finalize();
+    return 0;
+}

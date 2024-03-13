e0fc97797354fd91540703266b2f13f46a81cae0
Timotshak <TimEgor248@gmail.com>
2024-03-14 01:27:12 +0300

    multi-objective fitness functions

--- examples/hackathon/fitness.py
+++ examples/hackathon/fitness.py
@@ -22,3 +22,18 @@ class MultiFitness(FitnessFunction):
         if self._resources:
             fitness = (*fitness, Time.inf().value if schedule is None else count_resources(schedule))
         return fitness
+
+
+class WeightedFitness(FitnessFunction):
+    def __init__(self, time_weight: float = 0.5, cost_weight: float = 0.5, resources_weight: float = 0):
+        self._time = time_weight
+        self._cost = cost_weight
+        self._resources = resources_weight
+
+    def evaluate(self, chromosome: ChromosomeType, evaluator: Callable[[ChromosomeType], Schedule]) -> tuple[int]:
+        schedule = evaluator(chromosome)
+        if schedule is None:
+            return (Time.inf().value,)
+        return (self._time * schedule.execution_time + self._cost * schedule.pure_schedule_df['cost'].sum()
+                + self._resources * count_resources(schedule),)
+

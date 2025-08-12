2216ab76e4c84986367e5675b1a4286b8b619e76
Timotshak <TimEgor248@gmail.com>
2025-08-12 18:23:26 +0300

    fix lft with priorities

--- sampo/scheduler/lft/prioritization.py
+++ sampo/scheduler/lft/prioritization.py
@@ -1,4 +1,5 @@
-from operator import itemgetter
+from typing import Callable
+from itertools import chain
 
 import numpy as np
 import random
@@ -26,7 +27,12 @@ def lft_prioritization(head_nodes: list[GraphNode],
                        node_id2parent_ids: dict[str, set[str]],
                        node_id2child_ids: dict[str, set[str]],
                        node_id2duration: dict[str, int],
-                       core_f,
+                       core_f: Callable[[list[GraphNode],
+                                         dict[str, set[str]],
+                                         dict[str, set[str]],
+                                         dict[str, int],
+                                         list[list[GraphNode]],
+                                         random.Random], list[GraphNode]],
                        rand: random.Random() = random.Random()) -> list[GraphNode]:
     """
     Return ordered critical nodes.
@@ -35,10 +41,9 @@ def lft_prioritization(head_nodes: list[GraphNode],
 
     # form groups
     groups = extract_priority_groups_from_nodes(head_nodes)
+    groups = [groups[priority] for priority in sorted(groups.keys())]
 
-    result = []
-    for _, group in sorted(groups.items(), key=itemgetter(0)):
-        result.extend(core_f(group, node_id2parent_ids, node_id2child_ids, node_id2duration, rand))
+    result = core_f(head_nodes, node_id2parent_ids, node_id2child_ids, node_id2duration, groups, rand)
 
     return result
 
@@ -47,13 +52,15 @@ def lft_prioritization_core(head_nodes: list[GraphNode],
                             node_id2parent_ids: dict[str, set[str]],
                             node_id2child_ids: dict[str, set[str]],
                             node_id2duration: dict[str, int],
+                            groups: list[list[GraphNode]],
                             rand: random.Random = random.Random()) -> list[GraphNode]:
     """
     Return nodes ordered by MIN-LFT priority rule.
     """
     node_id2lft, _ = map_lft_lst(head_nodes, node_id2child_ids, node_id2duration)
 
-    ordered_nodes = sorted(head_nodes, key=lambda node: node_id2lft[node.id])
+    ordered_groups_it = (sorted(group, key=lambda node: node_id2lft[node.id]) for group in groups)
+    ordered_nodes = list(chain.from_iterable(ordered_groups_it))
 
     return ordered_nodes
 
@@ -62,25 +69,26 @@ def lft_randomized_prioritization_core(head_nodes: list[GraphNode],
                                        node_id2parent_ids: dict[str, set[str]],
                                        node_id2child_ids: dict[str, set[str]],
                                        node_id2duration: dict[str, int],
+                                       groups: list[list[GraphNode]],
                                        rand: random.Random = random.Random()) -> list[GraphNode]:
     """
     Return ordered nodes sampled by MIN-LFT and MIN-LST priority rules.
     """
-
-    head_nodes_set = set([node.id for node in head_nodes])
+    node_id2group_priority = {node.id: i for i, group in enumerate(groups) for node in group}
 
     def is_eligible(node_id):
-        return node_id2parent_ids[node_id].intersection(head_nodes_set).issubset(selected_ids_set)
+        return node_id2parent_ids[node_id].issubset(selected_ids_set)
 
     nodes2lft, nodes2lst = map_lft_lst(head_nodes, node_id2child_ids, node_id2duration)
 
     ordered_node_ids = []
     selected_ids_set = set()
-    candidates = {node.id for node in head_nodes
-                  if not node_id2parent_ids[node.id].intersection(head_nodes_set)}
+    candidates = {head_nodes[0].id}
 
     while candidates:
         eligibles = [node_id for node_id in candidates if is_eligible(node_id)]
+        min_group_priority = min(node_id2group_priority[node_id] for node_id in eligibles)
+        eligibles = [node_id for node_id in eligibles if node_id2group_priority[node_id] == min_group_priority]
 
         priority_mapper = nodes2lft if rand.random() < 0.5 else nodes2lst
 
@@ -93,11 +101,8 @@ def lft_randomized_prioritization_core(head_nodes: list[GraphNode],
         ordered_node_ids.append(selected_id)
         selected_ids_set.add(selected_id)
         candidates.remove(selected_id)
-        candidates.update([child_id for child_id in node_id2child_ids[selected_id] if child_id in head_nodes_set])
+        candidates.update([child_id for child_id in node_id2child_ids[selected_id]])
 
     ordered_nodes = sorted(head_nodes, key=lambda node: ordered_node_ids.index(node.id))
-
-    assert all(ordered_nodes[i].work_unit.priority <= ordered_nodes[i + 1].work_unit.priority
-               for i in range(len(ordered_nodes) - 1))
 
     return ordered_nodes
